c91da9e62f8b4150c9b32aa659814691c4a73228
chikipiki-djvu <kartinaybitoipechali@gmail.com>
2025-04-24 21:21:28 +0300

    Update task1.sql

--- task1.sql
+++ task1.sql
@@ -1,14 +1,1 @@
-SELECT name_author, SUM(amount) as Количество
-FROM 
-    author INNER JOIN book
-    on author.author_id = book.author_id
-GROUP BY name_author
-HAVING SUM(amount) = 
-     (/* вычисляем максимальное из общего количества книг каждого автора */
-      SELECT MAX(sum_amount) AS max_sum_amount
-      FROM 
-          (/* считаем количество книг каждого автора */
-            SELECT author_id, SUM(amount) AS sum_amount 
-            FROM book GROUP BY author_id
-          ) query_in
-      );
+
